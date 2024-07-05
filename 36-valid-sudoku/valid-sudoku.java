b04c3d4f9618b2c0abaf9f420bb3ec5d62aa2c46
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2024-07-05 03:38:56 -0500

    Time: 3 ms (59.98%) | Memory: 44.3 MB (53.28%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public boolean isValidSudoku(char[][] board) {
+        Set<Integer>[] rows = new HashSet[9];
+        Set<Integer>[] cols = new HashSet[9];
+        Set<Integer>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = Character.getNumericValue(board[i][j]);
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+
+
+                    if (!rows[i].add(num) || !cols[j].add(num) || !boxes[boxIndex].add(num)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
