0b34d8a022ae427b59c33b290a0ca894a1d34600
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2024-07-05 04:36:59 -0500

    Time: 131 ms (7.58%) | Memory: 46 MB (7.06%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = "";
+        for(char c: s.toLowerCase().toCharArray())
+            if(Character.isAlphabetic(c) || Character.isDigit(c))
+                str+=c;
+        for(int i=0;i<str.length()/2;i++)
+            if(str.charAt(i) == str.charAt(str.length()-1-i)){
+
+            }
+            else
+                return false;
+        return true;
+    }
+}
