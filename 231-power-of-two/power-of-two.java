a306a899813f44fcfac447980a9a517f8bac5318
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2024-07-05 22:25:57 -0500

    Time: 1 ms (40.97%) | Memory: 40.6 MB (64.64%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,15 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        boolean neg = false;;
         if(n<0)
             return false;
         for(int i=0;i<=31;i++){
             if((1<<i)==n){
-                if(neg)
-                    if(i%2==1)
-                        return true;
-                    else
-                        return false;
                 return true;
             }
         }
