63ab5c6fae28c8d2fc95997aac88234c78e9301e
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2024-07-15 04:09:14 -0500

    Time: 3 ms (78.62%) | Memory: 45.7 MB (52.16%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -1,24 +1,22 @@
 class Solution {
 
-    public List<String> adj(int n, int zero, int one, int idx,String ans){
+    public List<String> adj(int n, int zero, int one, int idx,String ans, List<String> res){
         if(idx==n){
-            List<String> res = new ArrayList<>();
+            //List<String> res = new ArrayList<>();
             res.add(ans);
-            System.out.println(ans);
             return res;
         }
-        List<String> res = new ArrayList<>();
         if(ans.length()==0)
-            res.addAll(adj(n, zero+1, one, idx+1, ans+"0"));
+            adj(n, zero+1, one, idx+1, ans+"0", res);
         if(ans.length()!=0)
             if(ans.charAt(ans.length()-1) != '0'){
-                res.addAll(adj(n, zero+1, one, idx+1, ans+"0"));
+                adj(n, zero+1, one, idx+1, ans+"0", res);
             }
-        res.addAll(adj(n, zero, one+1, idx+1, ans+"1"));
+        adj(n, zero, one+1, idx+1, ans+"1", res);
         return res;
     }
 
     public List<String> validStrings(int n) {
-        return adj(n, 0, 0, 0, "");
+        return adj(n, 0, 0, 0, "", new ArrayList<>());
     }
 }
