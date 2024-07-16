ba7f3d51e211090b34c08a3ff3c920bba36640ca
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2024-07-15 21:08:04 -0500

    Time: 1 ms (18.57%) | Memory: 42.3 MB (7.20%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,15 +1,12 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int res = 0,x=32;
-        String s = Integer.toBinaryString(n);
-        for(int i=s.length();i<32;i++)
-            s = "0"+s;
-        System.out.println(s);
+        int res = 0,b=0;
         for(int i=31; i>=0;i--){
-            x--;
-            if(s.charAt(i)=='1')
-                res += (1<<x);
+            int x = (n>>i)%2;
+            if( x==1 || x==-1)
+                res += (1<<b);
+            b++;
         }
         return res;
     }
