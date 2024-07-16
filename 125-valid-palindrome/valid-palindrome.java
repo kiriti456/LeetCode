a1c62c98882016a950c943d59a4ae099c426dd5d
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2024-07-16 00:29:39 -0500

    Time: 128 ms (9.37%) | Memory: 46.2 MB (5.99%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -5,10 +5,7 @@ class Solution {
             if(Character.isAlphabetic(c) || Character.isDigit(c))
                 str+=c;
         for(int i=0;i<str.length()/2;i++)
-            if(str.charAt(i) == str.charAt(str.length()-1-i)){
-
-            }
-            else
+            if(!(str.charAt(i) == str.charAt(str.length()-1-i)))
                 return false;
         return true;
     }
