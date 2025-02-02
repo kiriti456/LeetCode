3fe5648ae524f5265f5ed324a2ae4bb45f521a36
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2025-02-02 13:08:37 -0600

    Time: 1 ms (76.88%) | Memory: 42 MB (36.29%) - LeetSync

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.java
@@ -1,18 +1,18 @@
 class Solution {
     public String freqAlphabets(String s) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int i = 0;
         while (i < s.length()) {
             if (i + 2 < s.length() && s.charAt(i + 2) == '#') {
                 int num = Integer.parseInt(s.substring(i, i + 2));
-                result += ((char) (num + 96));
+                result.append((char) (num + 96));
                 i += 3;
             } else {
                 int num = s.charAt(i) - '0';
-                result += ((char) (num + 96));
+                result.append((char) (num + 96));
                 i++;
             }
         }
-        return result;
+        return result.toString();
     }
 }
