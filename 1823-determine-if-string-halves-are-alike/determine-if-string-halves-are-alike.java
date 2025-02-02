668095bbb963bc40066e050b0f6b3c52cb48c95d
kiriti456 <59302847+kiriti456@users.noreply.github.com>
2025-02-02 01:19:15 -0600

    Time: 6 ms (35.75%) | Memory: 41.6 MB (94.23%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int c = 0;
+        int c1 = 0;
         for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
             if(i <= (s.length()-1)/2){
-                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U')
-                    c++;
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                    c1++;
             }
             else{
-                if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U')
-                    c--;
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                    c1--;
             }
         }
-        return c == 0 ? true:false;
+        return c1 == 0 ? true:false;
     }
 }
